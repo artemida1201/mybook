8b05cc13eb0f75aaecfe7cefb7afe845b4a4c2ac
artemida1201 <122641512+artemida1201@users.noreply.github.com>
2023-01-13 22:43:12 +0300

    Add files via upload

---
+++ catalog/views.py
@@ -0,0 +1,60 @@
+import datetime
+
+from django.shortcuts import render
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact=2).count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+    return render(request, 'index.html', context={
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits
+    })
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+def timedelta_to_years(delta: datetime.timedelta) -> float:
+    seconds_in_year = 365.25 * 24 * 60 * 60
+    return delta.total_seconds() / seconds_in_year
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super(BookDetailView, self).get_context_data(**kwargs)
+        age = datetime.date.today() - self.request.user.profile.date_of_birth
+        context['age'] = int(timedelta_to_years(age))
+        return context
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/book_instance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects \
+            .filter(borrower=self.request.user) \
+            .filter(status__exact='7') \
+            .order_by('due_back')
